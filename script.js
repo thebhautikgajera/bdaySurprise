683aa8b6c40101d0620849a1763130616f77db91
bhautikpatel13 <bhautikpatel464@gmail.com>
2024-06-23 12:50:07 +0530

    2nd commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var countDownDate = new Date("July 01, 2024 00:00:00").getTime();
+
+var countdownfunction = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countDown").innerHTML =
+    days + "D " + hours + "H " + minutes + "M " + seconds + "S ";
+
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("countDown").innerHTML = "HAPPY BIRTHDAY SWEETY";
+    window.location = "https://www.instagram.com/the.bhautikk/";
+  }
+}, 1000);
