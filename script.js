553b1c0a2d10766fe94be6f580457e74de283e91
bhautikpatel13 <bhautikpatel464@gmail.com>
2024-06-30 21:21:00 +0530

    13th commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("July 01, 2024 00:00:00").getTime();
+var countDownDate = new Date("July 01, 2024 10:00:00").getTime();
 
 var countdownfunction = setInterval(function () {
   var now = new Date().getTime();
